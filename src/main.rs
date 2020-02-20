d611c46e637c1aa6f5d58213a1235e3c60af4371
Eric Olson <eric.rob.olson@gmail.com>
2020-02-20 08:15:03 -0800

    Started implementing OpenGL shaders.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate specs;
 use specs::prelude::*;
 extern crate sdl2;
 use sdl2::video::GLProfile;
+use std::ffi::{CStr, CString};
 
 // Internal crates
 #[macro_use]
@@ -60,7 +61,7 @@ fn main() {
     let mut dispatcher = DispatcherBuilder::new().build();
     dispatcher.setup(&mut world);
 
-    cb_simulation::assemblages::rts_assemblages::new_unit(&mut world);
+    //   cb_simulation::assemblages::rts_assemblages::new_unit(&mut world);
 
     loop {
         // Get Events
@@ -105,4 +106,49 @@ fn main() {
     }
 
     // Cleanup
+}
+
+fn shader_from_source(source: &CStr, kind: gl::types::GLuint) -> Result<gl::types::GLuint, String> {
+    let id = unsafe { gl::CreateShader(kind) };
+
+    unsafe {
+        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+        gl::CompileShader(id);
+    }
+
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+
+    const FAILURE_CODE: gl::types::GLint = 0;
+    if success == FAILURE_CODE {
+        // error handling
+        let mut len: gl::types::GLint = 0;
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        }
+
+        let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
+        buffer.extend([b' '].iter().cycle().take(len as usize));
+        let error = create_whitespace_cstring_with_len(len as usize);
+        unsafe {
+            gl::GetShaderInfoLog(
+                id,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut gl::types::GLchar,
+            );
+        }
+
+        return Err(error.to_string_lossy().into_owned());
+    }
+
+    return Ok(id);
+}
+
+fn create_whitespace_cstring_with_len(len: usize) -> CString {
+    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
+    buffer.extend([b' '].iter().cycle().take(len as usize));
+    return unsafe { CString::from_vec_unchecked(buffer) };
 }
