4c984cdeb7b5c57b7b4ed33c1a960caae66637f7
Eric Olson <eric.rob.olson@gmail.com>
2020-02-24 14:42:56 -0800

    implemented basic voxel.

--- src/cb_graphics/open_gl_backend/mod.rs
+++ src/cb_graphics/open_gl_backend/mod.rs
@@ -8,7 +8,84 @@ pub mod render_gl;
 
 pub struct OpenGlBackend {
     program: render_gl::Program,
-    vao: u32,
+    voxel_vao: u32,
+}
+
+fn init_cube_buffers() -> gl::types::GLuint {
+    let vertices: Vec<f32> = vec![
+        -1.0, -1.0, -1.0, // triangle 1 : begin
+        -1.0, -1.0, 1.0, // triangle 1: mid
+        -1.0, 1.0, 1.0, // triangle 1 : end
+        1.0, 1.0, -1.0, // triangle 2 : begin
+        -1.0, -1.0, -1.0, //
+        -1.0, 1.0, -1.0, // triangle 2 : end
+        1.0, -1.0, 1.0, // tri3
+        -1.0, -1.0, -1.0, //
+        1.0, -1.0, -1.0, // tri3
+        1.0, 1.0, -1.0, // tri4
+        1.0, -1.0, -1.0, //
+        -1.0, -1.0, -1.0, // tri4
+        -1.0, -1.0, -1.0, // tri begin
+        -1.0, 1.0, 1.0, //
+        -1.0, 1.0, -1.0, // tri end
+        1.0, -1.0, 1.0, // tri begin
+        -1.0, -1.0, 1.0, //
+        -1.0, -1.0, -1.0, // tri end
+        -1.0, 1.0, 1.0, // tri begin
+        -1.0, -1.0, 1.0, //
+        1.0, -1.0, 1.0, // tri end
+        1.0, 1.0, 1.0, // tri begin
+        1.0, -1.0, -1.0, //
+        1.0, 1.0, -1.0, // tri end
+        1.0, -1.0, -1.0, // tri begin
+        1.0, 1.0, 1.0, //
+        1.0, -1.0, 1.0, // tri end
+        1.0, 1.0, 1.0, // tri begin
+        1.0, 1.0, -1.0, //
+        -1.0, 1.0, -1.0, // tri end
+        1.0, 1.0, 1.0, // tri begin
+        -1.0, 1.0, -1.0, //
+        -1.0, 1.0, 1.0, // tri end
+        1.0, 1.0, 1.0, // tri begin
+        -1.0, 1.0, 1.0, //
+        1.0, -1.0, 1.0, // tri end
+    ];
+
+    let mut vbo_tri: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo_tri);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo_tri);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+            vertices.as_ptr() as *const gl::types::GLvoid,
+            gl::STATIC_DRAW,
+        );
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        let num_vertices = 12 * 3; // 12 triangles, 3 vertices each
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo_tri);
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (3 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            std::ptr::null(),
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
+    return vao;
 }
 
 impl OpenGlBackend {
@@ -26,53 +103,24 @@ impl OpenGlBackend {
         let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
         shader_program.set_used();
 
-        let vertices: Vec<f32> = vec![-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
-        let mut vbo: gl::types::GLuint = 0;
-        unsafe {
-            gl::GenBuffers(1, &mut vbo);
-
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-                vertices.as_ptr() as *const gl::types::GLvoid,
-                gl::STATIC_DRAW,
-            );
-
-            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        }
-
-        let mut vao: gl::types::GLuint = 0;
-        unsafe {
-            let num_vertices = 3;
-            gl::GenVertexArrays(1, &mut vao);
-            gl::BindVertexArray(vao);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl::EnableVertexAttribArray(0);
-            gl::VertexAttribPointer(
-                0,
-                num_vertices,
-                gl::FLOAT,
-                gl::FALSE,
-                (num_vertices as usize * std::mem::size_of::<f32>()) as gl::types::GLint,
-                std::ptr::null(),
-            );
-            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-            gl::BindVertexArray(0);
-        }
+        let voxels = init_cube_buffers();
 
         return Self {
             program: shader_program,
-            vao: vao,
+            voxel_vao: voxels,
         };
     }
 
     pub fn render(&mut self, game_state: &GameState) {
         self.program.set_used();
+        self.draw_voxel();
+        // draw triangle
+    }
 
+    fn draw_voxel(&mut self) {
         unsafe {
-            gl::BindVertexArray(self.vao);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::BindVertexArray(self.voxel_vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 12 * 3);
         }
     }
 }
